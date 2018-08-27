7c486e490d05332112c54e497643cb912d1deea4
Joao <jv.cruz@live.com>
2018-08-27 18:59:18 -0300

    Tenta achar a interssecção das hipérboles

--- Sym_03.m
+++ Sym_03.m
@@ -9,21 +9,35 @@ f1 = [1000 0];
 f2 = [-1000 0];
 f3 = [0 1000];
 p = [200 200];
- 
+
+plot(f1(1),f1(2),'b','Marker','o');
+hold on
+plot(f2(1),f2(2),'b','Marker','o');
+hold on
+plot(f3(1),f3(2),'b','Marker','o');
+hold on
+plot(p(1),p(2),'r*');
+hold on
+
 d1 = calc_dist(f1,p);
 d2 = calc_dist(f2,p);
 d3 = calc_dist(f3,p);
 
-for i=1:2
-t1 = d1/c + (rand*2 - 1)*error;
-t2 = d2/c + (rand*2 - 1)*error;
-t3 = d3/c + (rand*2 - 1)*error;
 
-hyperbola(f1,f2,t1,t2);
-hold on
-hyperbola(f2,f3,t2,t3);
-hold on
-hyperbola(f1,f3,t1,t3);
-hold on
+
+for i=1:2
+    t1 = d1/c + (rand*2 - 1)*error;
+    t2 = d2/c + (rand*2 - 1)*error;
+    t3 = d3/c + (rand*2 - 1)*error;
+
+    [h1,func1]=hyperbola(f1,f2,t1,t2);
+    hold on
+    [h2,func2]=hyperbola(f2,f3,t2,t3);
+    hold on
+    [h3,func3]=hyperbola(f1,f3,t1,t3);
+    hold on
 end
 grid on
+
+a = abs(func1-func2)<10;
+b=sum(a);
--- hyperbola.m
+++ hyperbola.m
@@ -1,4 +1,7 @@
-function [ hyp ] = hyperbola( p0,p1,t0,t1 )
+function [ hyp,f ] = hyperbola( p0,p1,t0,t1 )
+
+x = linspace(-1000,1000,1000*2/0.01);
+y = linspace(-1000,1000,1000*2/0.01);
 
 c = 299792458;
 
@@ -14,8 +17,8 @@ alfa = 2*(x2 - x1);
 beta = 2*(y2 - y1);
 gamma = x1^2 - x2^2 + y1^2 -y2^2 - 4*a^2;
 
-f =@(x,y) (alfa^2 - 16*a^2)*x^2 + (beta^2 - 16*a^2)*y^2 + 2*alfa*beta*x*y + 2*(alfa*gamma + 16*a^2*x2)*x + 2*(beta*gamma + 16*a^2*y2)*y + gamma^2 - 16*a^2*(x2^2 + y2^2);
-
-hyp = fimplicit(f,[-1000,1000,-1000,1000]);
+hyp = @(x,y) (alfa^2 - 16*a^2)*x^2 + (beta^2 - 16*a^2)*y^2 + 2*alfa*beta*x*y + 2*(alfa*gamma + 16*a^2*x2)*x + 2*(beta*gamma + 16*a^2*y2)*y + gamma^2 - 16*a^2*(x2^2 + y2^2);
+f = (alfa^2 - 16*a^2).*x.^2 + (beta^2 - 16*a^2).*y.^2 + 2*alfa*beta.*x.*y + 2*(alfa*gamma + 16*a^2*x2).*x + 2*(beta*gamma + 16*a^2*y2).*y + gamma^2 - 16*a^2*(x2^2 + y2^2);
+fimplicit(hyp,[-1000,1000,-1000,1000]);
 end
 
